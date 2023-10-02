03770cba6e02ce93d26b33208ffcdeca690429eb
Nathan Irwin <16235001@edu.cbe.ab.ca>
2023-10-02 10:27:55 -0600

    Binary Search

--- binary-search.js
+++ binary-search.js
@@ -7,19 +7,34 @@ function binarySearch(array, searchTerm) {
 	 ** repeat the above process for the subset of the array
 	 ** keep repeating with smaller and smaller subsets until you find the searchTerm
 	 */
-	let term = null;
-	
-	let middleIndex =  array.length / 2
-	// if (searchTerm == array[middleIndex]){
-	// 	return array[middleIndex]
-	// } else if (  ){
 
-	// }
+	let newArray = function(a, b1, b2){
+		//b1 must be smaller then b2
+		let newArray = [];
+		for (let i = b1; i < b2; i++) {
+			const element = a[i];
+			newArray.push(element)
+		}
+		return newArray
+	}
+
+	let term;
+	let middleIndex = Math.floor(array.length / 2);
+
+	if (searchTerm == array[middleIndex]){
+		console.log("found");
+		term = array[middleIndex];
+	} else if ( searchTerm > array[middleIndex] ){
+		binarySearch(newArray(array, middleIndex, array.length), searchTerm)
+	} else { 
+		binarySearch(newArray(array, 0, middleIndex + 1), searchTerm)
+	}
 	
-	
-	return term;
+	console.log(term)
+	// return term
+
 }
 
-//console.log(binarySearch([1,2,3,4,5,6,7, 8], 2))
+console.log(binarySearch([1,2,3,4,5,6,7, 8], 2));
 
 module.exports = binarySearch;
